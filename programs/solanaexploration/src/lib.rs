1ad1811695231e193d99b5b39701163fa803a8e4
Obed Tandadjaja <obed.tandadjaja@gmail.com>
2021-11-09 23:54:03 -0500

    added add gif function

--- programs/solanaexploration/src/lib.rs
+++ programs/solanaexploration/src/lib.rs
@@ -12,6 +12,12 @@ pub mod solanaexploration {
         base_account.total_gifs = 0;
         Ok(())
     }
+
+    pub fn add_gif(ctx: Context<AddGif>) -> ProgramResult {
+        let base_account = &mut ctx.accounts.base_account;
+        base_account.total_gifs += 1;
+        Ok(())
+    }
 }
 
 #[derive(Accounts)]
@@ -33,6 +39,12 @@ pub struct Initialize<'info> {
     // reference to SystemProgram which is responsible to create accounts
     // in Solana.
     pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct AddGif<'info> {
+    #[account(mut)]
+    pub base_account: Account<'info, BaseAccount>,
 }
 
 #[account]
