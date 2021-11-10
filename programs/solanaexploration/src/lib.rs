12f4fbc9ebe4235444896b722a1db1bc0e71e897
Obed Tandadjaja <obed.tandadjaja@gmail.com>
2021-11-09 23:47:00 -0500

    made changes to solana program

--- programs/solanaexploration/src/lib.rs
+++ programs/solanaexploration/src/lib.rs
@@ -6,9 +6,36 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod solanaexploration {
     use super::*;
     pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
+        // Get a reference to the account
+        let base_account = &mut ctx.accounts.base_account;
+        // initialize total_gifs
+        base_account.total_gifs = 0;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    // init - tell solana to create new account
+    // payer = user - payer of the account is user
+    // space = 9000 - allocate 9000 bytes for account
+    //
+    // note that solana users pay to rent their accounts. If you don't pay
+    // rent then validators will clear the account.
+    #[account(init, payer = user, space = 9000)]
+    pub base_account: Account<'info, BaseAccount>,
+
+    // data passed to the program that proves that the user calling
+    // actually owns their wallet account
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    // reference to SystemProgram which is responsible to create accounts
+    // in Solana.
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct BaseAccount {
+    pub total_gifs: u64,
+}
